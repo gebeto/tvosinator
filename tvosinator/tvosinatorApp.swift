050eaa0a688721295f4e8bd357d0bc7cabff66c1
gebeto <slavik.nychkalo@gmail.com>
2021-09-04 16:56:28 +0300

    Initial Commit

---
+++ tvosinator/tvosinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvosinatorApp.swift
+//  tvosinator
+//
+//  Created by gebeto on 04.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct tvosinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
